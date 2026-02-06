2401caf2d27f34c4d39032363561079c4758ce34
Pavana <pavanapoojary2004@gmail.com>
2026-02-06 16:34:20 +0530

    Create main1.py

---
+++ src/Day-5/main1.py
@@ -0,0 +1,7 @@
+#Task-2
+import math_operations
+result_power = math_operations.power(2, 10)
+print("2^10 =", result_power)
+nums = [10, 20, 30, 40]
+result_avg = math_operations.average(nums)
+print("Average =", result_avg)
